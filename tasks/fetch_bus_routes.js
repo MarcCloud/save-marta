e60c7e3a985a78ad4a5497905ab27c34c2362081
Marco Romero <jmarco.romero@protonmail.com>
2020-04-01 11:34:40 -0500

    Add script to load routes data on npm start

---
+++ tasks/fetch_bus_routes.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+
+const MARTA_URL = "http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus";
+
+function transformJson(rawData = []){
+    return rawData.map((rawRoute) => {
+        const { ROUTE, TIMEPOINT, LATITUDE, LONGITUDE } = rawRoute;
+        return {
+            lat: LATITUDE,
+            long: LONGITUDE,
+            route: ROUTE,
+            timepoint: TIMEPOINT
+        }
+    })
+}
+
+function saveToData(busRoutes = []){
+    try {
+        const content = JSON.stringify(busRoutes);
+        fs.writeFile('data/bus_routes.json',content , (err) => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            console.log('Bus Routes Data Saved:', content);
+          })
+        
+      } catch (err) {
+        console.error(err)
+      }
+}
+
+fetch(MARTA_URL)
+    .then((res) => res.json())
+    .then((rawData) => transformJson(rawData))
+    .then(saveToData)
